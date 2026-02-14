e9ef9136fd776990ac21eba683bb8443a6998c3a
selam-mbk1 <selamawitbasaznew@gmail.com>
2026-02-15 05:01:46 +1200

    Fix app_name errer

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'accounts'  # optional but good practice for namespacing
+app_name = 'accounts'  
 
 urlpatterns = [
     path('', views.home, name='home'),                   
